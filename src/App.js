5fd08c8771fd02f664ffaf33064cd659e94b81f2
nmariahpaul <npaul@mtroyal.ca>
2022-10-07 22:48:10 -0600

    Created weather draft homepage
    Included footer with link to github repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Weather Project</h1>
+      <footer>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This project was coded by Mariah Paul, and is{" "}
+          <a href="" target="_blank" rel="noreferrer" className="github-link">
+            open-sourced on Github
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
   );
 }
