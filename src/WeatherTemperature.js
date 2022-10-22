32a50972da0ee0609650364e85cc67db211ce103
nmariahpaul <npaul@mtroyal.ca>
2022-10-21 21:03:33 -0600

    Included useEffect for forecast

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,50 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Weather.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit((props.celsius * 9) / 5 + 32);
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <span className="Weather">
-        <span className="temperature">
-          <strong>{Math.round(props.celsius)}</strong>
-        </span>
-        <span className="units">
-          °C |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
+  return (
+    <span className="Weather">
+      <span className="temperature">
+        <strong>{Math.round(props.celsius)}</strong>
       </span>
-    );
-  } else {
-    return (
-      <span className="Weather">
-        <span className="temperature">
-          <strong>{Math.round(fahrenheit())}</strong>
-        </span>
-        <span className="units">
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </span>
-    );
-  }
+      <span className="units">°C</span>
+    </span>
+  );
 }
